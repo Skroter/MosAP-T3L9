f97814d1bef617f87604de5c04082e5c71d4aa53
Skroter <digro@yandex.ru>
2024-06-28 23:00:46 +0500

    Добавьте файлы проекта.

---
+++ Topic3Lesson9Task3/Topic3Lesson9Task3.py
@@ -0,0 +1,9 @@
+﻿l = {1, 2, 3, 4, 5}
+
+arr = list(map(int, input().split()))
+
+for i in range(len(arr)):
+    if arr[i] in l:
+        print("Yes")
+    else:
+        print("No")
---
+++ test/test.py
@@ -0,0 +1,18 @@
+﻿from cgi import test
+import random
+
+N = 0
+while N == 0:
+    print('Введите число в диапазоне 1 - 100_000:')
+    testN = int(input())
+    if testN < 1:
+        print('Введите число заново')
+    elif testN > 100_000:
+        print('Введите число заново')
+    else:
+        N = testN 
+print(N)
+# x = 2*10_000_000_000 
+# arr = [random.randint(1, x) for i in range(N)] 
+# many = set(arr) 
+# print(len(many)) 
